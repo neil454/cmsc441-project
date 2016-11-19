e7f848645f5fd89bf2026e29e179a321ed35bf1c
neil <neil454@gmail.com>
2016-11-19 01:59:33 -0500

    Project 2 - Multithreaded Algo, mainly finished
    Added iterative port of project 1 algo in python, for prep for project 2
    Ported the above iterative python code into c++, and added an OpenMP pragma statement to enable multithreading
    Along with the cpp file, there is a CMakeLists.txt for cmake use

---
+++ project2.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <omp.h>
+#include <time.h>
+
+using namespace std;
+
+// Declare global variables that functions can access (this way, we don't have to pass them as args)
+string a;   // the line representation, which is a string on the alphabet, {H, G, W, T}
+std::vector<std::vector<int>> opt_pairs_table;      // table for storing memoized outputs of OPT for all i and j pairs
+
+// declare function prototypes
+int OPT_iter(int i, int j);
+int OPT(int i, int j);
+
+int OPT_iter(int i, int j){
+    int opt_ret = 0;
+    for (int r = 0; r < a.size(); r++){
+        #pragma omp parallel for
+        for (int c = 0; c < a.size() - r; c++){
+            opt_ret = OPT(c, c+r);
+        }
+    }
+
+    return opt_pairs_table[i][j];
+}
+
+int OPT(int i, int j){
+    // handle case of invalid (i, j) values
+    if ((i < 0) || (j < 0) || (i > a.size() - 1) || (j > a.size() - 1)){
+        return 0;
+    }
+
+    // Use memoized result, if available
+    if (opt_pairs_table[i][j] != -1){
+        return opt_pairs_table[i][j];
+    }
+
+    // To avoid sharp turns, we don't look for pairs when i and j are less than or equal to 4 spaces away from each other
+    if (i >= (j-4)){
+        opt_pairs_table[i][j] = 0;
+        return 0;
+    }
+
+    // Set the max optimal number of pairs to OPT(i, j-1), in case no optimal t is found that beats it
+    // NOTE: Because this iterative algorithm is bottom up, and thread-safe, the recursive call below
+    // should not call any other recursive calls, that is, it should have a memoized value already
+    int max_opt = OPT(i, j-1);
+
+    // Look for optimal pairing for a[j] (which we call a[t]), by going through all possible t's
+    for (int t = i; t < (j-4); t++){
+        // check if valid pairing
+        string pair = string() + a[t] + a[j];
+        if ((pair == "HG") || (pair == "GH") ||(pair == "WT") ||(pair == "TW")){
+            // recursively call OPT for the line before t, for the line after t, and add 1 to account for a[t] itself
+            // NOTE: Because this iterative algorithm is bottom up, and thread-safe, any of the recursive calls below
+            // should not call any other recursive calls, that is, they should have memoized values already
+            int opt = (OPT(i, t-1)) + (OPT(t+1, j-1)) + 1;
+            // if the optimal number of pairs for this choice of t is larger than max_opt, make it max_opt
+            if (opt >= max_opt){
+                max_opt = opt;
+            }
+        }
+    }
+
+    // Memoize this result so we don't have to recompute it again, then return it
+    opt_pairs_table[i][j] = max_opt;
+    return max_opt;
+}
+
+string get_line_from_file(string file_name, int n){
+    ifstream infile;
+    infile.open(file_name);
+    if (infile.good() == 0){
+        cout << "ERROR: file_name, " << file_name << " doesn't exist." << endl;
+        return "";
+    }
+    string line;
+    getline(infile, line);
+    return line.substr(0, n);
+}
+
+int get_num_pairs_for_line(int n){
+    // Initialize the line representation, a, which is a string on the alphabet, {H, G, W, T}
+    a = get_line_from_file("test_data.txt", n);
+
+    // Initialize a n-by-n 2D array of -1s, for storing memoized outputs of OPT for all i and j pairs
+    vector<vector<int>> mat(n, vector<int>(n, -1));
+    opt_pairs_table = mat;
+
+    // Calculate the optimal pairs for the line, and time the function
+    double start_time = omp_get_wtime();
+    int opt_num_pairs = OPT_iter(0, n-1);
+    double time = omp_get_wtime() - start_time;
+
+    cout << "n = " << n << ", OPT(1, n) = " << opt_num_pairs << " (Done in " << time << " seconds)" << endl;
+
+    return opt_num_pairs;
+}
+
+int main(){
+    for (int n = 50; n < 1000; n+=50){
+        get_num_pairs_for_line(n);
+    }
+    for (int n = 1000; n <= 2000; n+=200){
+        get_num_pairs_for_line(n);
+    }
+}
---
+++ project2_iterative_port.py
@@ -0,0 +1,97 @@
+import sys
+import time
+import random
+
+from joblib import Parallel, delayed
+import multiprocessing
+
+
+# Declare global variables that functions can access, if they need them (this way, we don't have to pass them as args)
+a = None                    # the line representation, which is a string on the alphabet, {H, G, W, T}
+opt_pairs_table = None      # table for storing memoized outputs of OPT for all i and j pairs
+
+def OPT_iter(i, j):
+    global a
+
+    for r in range(len(a)):
+        # opt_ret = Parallel(n_jobs=4)(delayed(OPT)(c, c+r) for c in range(len(a) - r))
+        for c in range(len(a) - r):
+            opt_ret = OPT(c, c+r)
+
+    return opt_ret
+
+def OPT(i, j, in_call=False):
+    global a, opt_pairs_table
+
+    if i < 0 or j < 0 or i > len(a) - 1 or j > len(a) - 1:
+        return 0
+
+    # Use memoized result, if available
+    if opt_pairs_table[i][j] != -1:
+        return opt_pairs_table[i][j]
+
+    # To avoid sharp turns, we don't look for pairs when i and j are less than or equal to 4 spaces away from each other
+    if i >= (j-4):
+        opt_pairs_table[i][j] = 0
+        return 0
+
+    if in_call:
+        print "REALLY BAD", i, j
+
+    # Set the max optimal number of pairs to OPT(i, j-1), in case no optimal t is found that beats it
+    max_opt = OPT(i, j-1)
+
+    # Look for optimal pairing for a[j] (which we call a[t]), by going through all possible t's
+    for t in range(i, j-4):
+        # check if valid pairing
+        if a[t] + a[j] in ["HG", "GH", "WT", "TW"]:
+            # recursively call OPT for the line before t, for the line after t, and add 1 to account for a[t] itself
+            opt = (OPT(i, t-1, in_call=True)) + (OPT(t+1, j-1, in_call=True)) + 1
+            # if the optimal number of pairs for this choice of t is larger than max_opt, make it max_opt
+            if opt >= max_opt:
+                max_opt = opt
+
+    # Memoize this result so we don't have to recompute it again, then return it
+    opt_pairs_table[i][j] = max_opt
+    return max_opt
+
+def get_random_line(n):
+    return ''.join(random.choice("HGWT") for _ in xrange(n))
+
+
+def get_line_from_file(file_name, n):
+    return open(file_name).readline()[:n]
+
+def main():
+    global a, opt_pairs_table
+    # Initialize the line representation, a, which is a string on the alphabet, {H, G, W, T}
+    # a = "HGGTHWHWWHTG"
+
+    line_length = 500
+    # a = get_random_line(n=line_length)
+    a = get_line_from_file("test_data.txt", n=line_length)
+
+    n = len(a)
+    # python's recursion depth limit is 1000, so increase it if we need to
+    if n > 1000:
+        sys.setrecursionlimit(n)
+
+    # Initialize a n-by-n 2D array of -1s, for storing memoized outputs of OPT for all i and j pairs
+    # This is done in theta(n^2) time
+    opt_pairs_table = [[-1 for col in range(n)] for row in range(n)]
+    # for i in range(n):
+    #     for j in range(n):
+    #         if i >= (j-4):
+    #             opt_pairs_table[i][j] = 0
+
+    # Calculate the optimal pairs for whole line, which will make recursive calls to subproblems
+    start_time = time.time()
+    opt_num_pairs = OPT_iter(0, n - 1)
+    end_time = time.time()
+    print "n = " + str(n) + ", OPT(1, n) = " + str(opt_num_pairs),
+    print "(Done in", end_time - start_time, "seconds)"
+
+    return 0
+
+if __name__ == '__main__':
+    main()
